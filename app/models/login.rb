4830cf8b3da67fea3022b2deebb48c5ddbde28e0
Britny Herzog <britny@bitium.com>
2015-08-07 16:53:05 -0700

    More ip addresses

--- app/models/login.rb
+++ app/models/login.rb
@@ -8,7 +8,12 @@ class Login < ActiveRecord::Base
     '-79.3872',
     '-118.4143',
     '-97.0000',
-    '2.1333'
+    '2.1333',
+    '106.8294',
+    '133.0000',
+    '-88.3420',
+    '-0.0931'
+
   ]
   LAT_ARRAY = [
     '33.759',
@@ -18,14 +23,19 @@ class Login < ActiveRecord::Base
     '43.6425',
     '34.0995',
     '38.0000',
-    '47.9333'
+    '47.9333',
+    '-6.1744',
+    '-27.0000',
+    '44.2088',
+    '51.5142'
+
   ]
 
   before_create :setup
 
   def setup
     self.ip = '2423423423'
-    num = rand(8)
+    num = rand(12)
     self.lat =  LAT_ARRAY[num]
     self.lon =  LON_ARRAY[num]
   end
